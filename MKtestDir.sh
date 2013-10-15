8b160630121439518308483cf1a49a10e1cc66d2
gl237man <gl237man@gmail.com>
2013-10-15 20:48:09 +0400

    Исправил синтаксис в MKtestdir.sh

--- MKtestDir.sh
+++ MKtestDir.sh
@@ -2,12 +2,12 @@
 # Создание тестовой директории
 ALTERA_BIN=/home/gl237/altera/13.0sp1/quartus/linux64/
 MINEROUTE_PATH=/home/gl237/mineroute/mineroute/
-mkdir test
-cp ./Templates/*.binhl ./test/
-cp ./Binhl2JsWE/bin/Debug/*.exe ./test/
-cp ./vqm2MNET/bin/Debug/*.exe ./test/
-cp ./MNetSynt/bin/Debug/*.exe ./test/
-cp ./EDF2MNET/bin/Debug/*.exe ./test/
-cp ./RouteUtils/bin/Debug/*.dll ./test/
-export LD_LIBRARY_PATH=$(ALTERA_BIN)
-$(ALTERA_BIN)quartus_cdb $(MINEROUTE_PATH)TestQuartusProject/test -vqm=$(MINEROUTE_PATH)test/test.vqm
+mkdir test -v
+cp ./Templates/*.binhl ./test/ -v
+cp ./Binhl2JsWE/bin/Debug/*.exe ./test/ -v
+cp ./vqm2MNET/bin/Debug/*.exe ./test/ -v
+cp ./MNetSynt/bin/Debug/*.exe ./test/ -v
+cp ./EDF2MNET/bin/Debug/*.exe ./test/ -v
+cp ./RouteUtils/bin/Debug/*.dll ./test/ -v
+export LD_LIBRARY_PATH=${ALTERA_BIN}
+${ALTERA_BIN}quartus_cdb ${MINEROUTE_PATH}TestQuartusProject/test -vqm=${MINEROUTE_PATH}test/test.vqm
