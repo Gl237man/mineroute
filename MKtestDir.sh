ece4c2b2a547edeeee744930638f5665170ca39e
Vadim <gl237man@gmail.com>
2013-10-15 09:56:46 +0400

    Добавил коментарии, вынес пути в переменные

--- MKtestDir.sh
+++ MKtestDir.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+# Создание тестовой директории
+ALTERA_BIN=/home/gl237/altera/13.0sp1/quartus/linux64/
+MINEROUTE_PATH=/home/gl237/mineroute/mineroute/
 mkdir test
 cp ./Templates/*.binhl ./test/
 cp ./Binhl2JsWE/bin/Debug/*.exe ./test/
@@ -5,5 +9,5 @@ cp ./vqm2MNET/bin/Debug/*.exe ./test/
 cp ./MNetSynt/bin/Debug/*.exe ./test/
 cp ./EDF2MNET/bin/Debug/*.exe ./test/
 cp ./RouteUtils/bin/Debug/*.dll ./test/
-export LD_LIBRARY_PATH=/home/gl237/altera/13.0sp1/quartus/linux64/
-/home/gl237/altera/13.0sp1/quartus/linux64/quartus_cdb /home/gl237/mineroute/mineroute/TestQuartusProject/test -vqm=/home/gl237/mineroute/mineroute/test/test.vqm
+export LD_LIBRARY_PATH=$ALTERA_BIN
+$(ALTERA_BIN)quartus_cdb $(MINEROUTE_PATH)TestQuartusProject/test -vqm=$(MINEROUTE_PATH)test/test.vqm
