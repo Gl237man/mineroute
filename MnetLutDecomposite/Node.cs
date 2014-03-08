06b006fb97ae56ab653b22a504ac49627a34809a
Gl237man <gl237man@mail.ru>
2014-03-09 01:05:35 +0400

    Добавил проект MineLutDecomposite для разложения LUT в набор гейтов

---
+++ MnetLutDecomposite/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MnetLutDecomposite
+{
+    class Node
+    {
+        public string NodeType;
+        public string NodeName;
+        public string ToString()
+        {
+            return "NODE:" + NodeType + ":" + NodeName;
+        }
+        public void ReadFromString(string instr)
+        {
+            string[] tstr = instr.Split(':');
+            NodeType = tstr[1];
+            NodeName = tstr[2];
+        }
+    }
+}
---
+++ MnetLutDecomposite/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MnetLutDecomposite
+{
+    class Program
+    {
+        static List<Node> nodes;
+        static List<Wire> wires;
+        static void Main(string[] args)
+        {
+            ReadMnetFile(@"test2.MNET");
+        }
+
+        private static void ReadMnetFile(string FileName)
+        {
+            nodes = new List<Node>();
+            wires = new List<Wire>();
+            string[] tstr = System.IO.File.ReadAllLines(FileName);
+            for (int i=0;i<tstr.Length;i++)
+            {
+                if (tstr[i].Split(':')[0] == "NODE")
+                {
+                    Node N = new Node();
+                    N.ReadFromString(tstr[i]);
+                    nodes.Add(N);
+                }
+                if (tstr[i].Split(':')[0] == "WIRE")
+                {
+                    Wire W = new Wire();
+                    W.ReadFromString(tstr[i]);
+                    wires.Add(W);
+                }
+            }
+        }
+    }
+}
