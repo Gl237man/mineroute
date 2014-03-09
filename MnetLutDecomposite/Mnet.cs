0741e53fc5640a037ab0d688ad1923d96ef004c3
Vadim <gl237man@mail.ru>
2014-03-09 13:08:50 +0400

    Вынес загрузки в отдельный класс

---
+++ MnetLutDecomposite/Mnet.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MnetLutDecomposite
+{
+    class Mnet
+    {
+        public List<Node> nodes;
+        public List<Wire> wires;
+
+        public void ReadMnetFile(string FileName)
+        {
+            nodes = new List<Node>();
+            wires = new List<Wire>();
+            string[] tstr = System.IO.File.ReadAllLines(FileName);
+            for (int i = 0; i < tstr.Length; i++)
+            {
+                if (tstr[i].Split(':')[0] == "NODE")
+                {
+                    Node N = new Node();
+                    N.ReadFromString(tstr[i]);
+                    nodes.Add(N);
+                }
+                if (tstr[i].Split(':')[0] == "WIRE")
+                {
+                    Wire W = new Wire();
+                    W.ReadFromString(tstr[i]);
+                    wires.Add(W);
+                }
+            }
+        }
+    }
+}
--- MnetLutDecomposite/Program.cs
+++ MnetLutDecomposite/Program.cs
@@ -6,33 +6,15 @@ namespace MnetLutDecomposite
 {
     class Program
     {
-        static List<Node> nodes;
-        static List<Wire> wires;
+        static Mnet MainNet;
+
         static void Main(string[] args)
         {
-            ReadMnetFile(@"test2.MNET");
+            MainNet = new Mnet();
+            MainNet.ReadMnetFile(@"test2.MNET");
+
         }
 
-        private static void ReadMnetFile(string FileName)
-        {
-            nodes = new List<Node>();
-            wires = new List<Wire>();
-            string[] tstr = System.IO.File.ReadAllLines(FileName);
-            for (int i=0;i<tstr.Length;i++)
-            {
-                if (tstr[i].Split(':')[0] == "NODE")
-                {
-                    Node N = new Node();
-                    N.ReadFromString(tstr[i]);
-                    nodes.Add(N);
-                }
-                if (tstr[i].Split(':')[0] == "WIRE")
-                {
-                    Wire W = new Wire();
-                    W.ReadFromString(tstr[i]);
-                    wires.Add(W);
-                }
-            }
-        }
+        
     }
 }
