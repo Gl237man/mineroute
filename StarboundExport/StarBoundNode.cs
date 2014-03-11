d07a2ec80b9f177c8acdf4ff5ce0c4b3c5b62066
Vadim <gl237man@gmail.com>
2014-03-11 09:33:22 +0300

    Дописал satrboundNode

--- StarboundExport/StarBoundNode.cs
+++ StarboundExport/StarBoundNode.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +8,20 @@ namespace StarboundExport
 {
     class StarBoundPort
     {
+        private int xshift;
+        private int yshift;
         public string PortName;
-        public int xcoord;
-        public int ycoord;
+        public int xcoord
+        {
+            get { return NodeOwner.xcoord + xshift; }
+            set { xshift = value; }
+        }
+        public int ycoord
+        {
+            get { return NodeOwner.ycoord + yshift; }
+            set { yshift = value; }
+        }
+        public StarBoundNode NodeOwner;
     }
     class StarBoundNode
     {
@@ -19,6 +30,66 @@ namespace StarboundExport
         public int xcoord;
         public int ycoord;
         public List<StarBoundPort> Ports;
- 
+
+        public StarBoundNode(string type, string ID)
+        {
+            
+            Ports = new List<StarBoundPort>();
+            NodeID = ID;
+            xcoord = 0;
+            ycoord = 0;
+            switch (type)
+            {
+                case "GND":
+                    NodeType = "OR_GATE";
+                    //make ports
+                    Ports.Add(new StarBoundPort { PortName = "O0", xcoord = 2, ycoord = 0, NodeOwner = this });
+                    break;
+                case "VCC":
+                    NodeType = "NOT_GATE";
+                    //make ports
+                    Ports.Add(new StarBoundPort { PortName = "O0", xcoord = 2, ycoord = 0, NodeOwner = this });
+                    break;
+                case "INPort":
+                    NodeType = "BUTTON";//В будующем заменю на переключатели или двери если нет переключателей
+                    //make ports
+                    Ports.Add(new StarBoundPort { PortName = "O0", xcoord = 1, ycoord = 1 ,NodeOwner = this});
+                    break;
+                case "OUTPort":
+                    NodeType = "BLUB";
+                    //make ports
+                    Ports.Add(new StarBoundPort { PortName = "O0", xcoord = 0, ycoord = 0 ,NodeOwner = this});
+                    break;
+                case "TRIG_D":
+                    NodeType = "D_TRIG";
+                    //make ports
+                    Ports.Add(new StarBoundPort { PortName = "clk", xcoord = 0, ycoord = -1, NodeOwner = this });
+                    Ports.Add(new StarBoundPort { PortName = "datain", xcoord = 0, ycoord = 1, NodeOwner = this });
+                    Ports.Add(new StarBoundPort { PortName = "regout", xcoord = 2, ycoord = 0, NodeOwner = this });
+                    break;
+                case "OR":
+                    NodeType = "OR_GATE";
+                    //make ports
+                    Ports.Add(new StarBoundPort { PortName = "I0", xcoord = 0, ycoord = -1, NodeOwner = this });
+                    Ports.Add(new StarBoundPort { PortName = "I1", xcoord = 0, ycoord = 1, NodeOwner = this });
+                    Ports.Add(new StarBoundPort { PortName = "O0", xcoord = 2, ycoord = 0, NodeOwner = this });
+                    break;
+                case "AND":
+                    NodeType = "AND_GATE";
+                    //make ports
+                    Ports.Add(new StarBoundPort { PortName = "I0", xcoord = 0, ycoord = -1, NodeOwner = this });
+                    Ports.Add(new StarBoundPort { PortName = "I1", xcoord = 0, ycoord = 1, NodeOwner = this });
+                    Ports.Add(new StarBoundPort { PortName = "O0", xcoord = 2, ycoord = 0, NodeOwner = this });
+                    break;
+                case "NOT":
+                    NodeType = "NOT_GATE";
+                    //make ports
+                    Ports.Add(new StarBoundPort { PortName = "I0", xcoord = 0, ycoord = 0, NodeOwner = this });
+                    Ports.Add(new StarBoundPort { PortName = "O0", xcoord = 2, ycoord = 0, NodeOwner = this });
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
