f4fda4eb37aec8dae2fe05b1a5058df1d92affce
Vadim <gl237man@gmail.com>
2014-03-12 09:07:21 +0300

    Добавил оптимизацию расположения элементов

--- StarboundExport/Program.cs
+++ StarboundExport/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
 
@@ -24,17 +24,110 @@ namespace StarboundExport
             List<Node> DupNodes = new List<Node>();
             RemoveDUPNodes(MainNetwork, DupNodes);
             RemoveDUOWires(MainNetwork, DupNodes);
+
+            //Sorting Nodes;
+            SortOptimize(MainNetwork);
+
             //place nods
             List<StarBoundNode> SBNodes = new List<StarBoundNode>();
-
             PlaceSBNodes(MainNetwork, SBNodes);
             //place wires
             List<StarboundWire> SBWires = new List<StarboundWire>();
             ConnectSBNodes(MainNetwork, SBNodes, SBWires);
             string ostr = GenExportFile(SBNodes, SBWires);
             System.IO.File.WriteAllText(file + ".SBBIN", ostr);
+            //CalcLincLengch
+            double len = 0;
+            for (int i = 0; i < SBWires.Count; i++)
+            {
+                len += Math.Sqrt((SBWires[i].startx - SBWires[i].endx) * (SBWires[i].startx - SBWires[i].endx)
+                               + (SBWires[i].starty - SBWires[i].endy) * (SBWires[i].starty - SBWires[i].endy));
+            }
+            Console.WriteLine("complexity:" + len);
             //TODO
             //draw image
+            Image IMmain = DrawImage(SBNodes, SBWires);
+            //export image
+            IMmain.Save(file + ".bmp");
+
+        }
+
+        private static void SortOptimize(Mnet MainNetwork)
+        {
+            for (int i = 0; i < MainNetwork.nodes.Count * 5; i++)
+            {
+                for (int j = 0; j < (MainNetwork.nodes.Count - 1); j++)
+                {
+                    if (MainNetwork.nodes[j].NodeType != "INPort" && MainNetwork.nodes[j].NodeType != "OUTPort")
+                    {
+                        List<Wire> Wlist1 = FindAllWiresTo(MainNetwork.wires, MainNetwork.nodes[j]);
+                        //List<Wire> Wlist2 = FindAllWiresTo(MainNetwork.wires, MainNetwork.nodes[j+1]);
+                        int ka = CalcWireLens(Wlist1, MainNetwork);
+                        //int kb = CalcWireLens(Wlist2, MainNetwork);
+                        if (ka < 0)
+                        {
+                            if (MainNetwork.nodes[j + 1].NodeType != "INPort" && MainNetwork.nodes[j + 1].NodeType != "OUTPort")
+                            {
+                                Node N = new Node();
+                                N = MainNetwork.nodes[j];
+                                MainNetwork.nodes[j] = MainNetwork.nodes[j + 1];
+                                MainNetwork.nodes[j + 1] = N;
+                            }
+                        }
+                        if (ka > 0)
+                        {
+                            if (MainNetwork.nodes[j - 1].NodeType != "INPort" && MainNetwork.nodes[j - 1].NodeType != "OUTPort")
+                            {
+                                Node N = new Node();
+                                N = MainNetwork.nodes[j];
+                                MainNetwork.nodes[j] = MainNetwork.nodes[j - 1];
+                                MainNetwork.nodes[j - 1] = N;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static int CalcWireLens(List<Wire> WlistTo, Mnet MainNetwork)
+        {
+            int k = 0;
+            for (int i = 0; i < WlistTo.Count; i++)
+            {
+                int ka = FindNodeIndex(MainNetwork.nodes, WlistTo[i].SrcName);
+                int kb = FindNodeIndex(MainNetwork.nodes, WlistTo[i].DistName);
+                k += (kb - ka);
+            }
+            return k;
+        }
+
+        private static int FindNodeIndex(List<Node> list, string p)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].NodeType == "OUTPort")
+                    return list.Count;
+                if (list[i].NodeName == p)
+                    return i;
+            }
+            return 0;
+        }
+
+        private static List<Wire> FindAllWiresTo(List<Wire> Wlist, Node node)
+        {
+            List<Wire> WO = new List<Wire>();
+            for (int i = 0; i < Wlist.Count; i++)
+            {
+                if (Wlist[i].DistName == node.NodeName)
+                    WO.Add(Wlist[i]);
+                if (Wlist[i].SrcName == node.NodeName)
+                    WO.Add(Wlist[i]);
+            }
+            return WO;
+        }
+
+        private static Image DrawImage(List<StarBoundNode> SBNodes, List<StarboundWire> SBWires)
+        {
             Image IMmain = new Bitmap((tx + 2) * ImageMult * 3, (ty + 2) * ImageMult * 3);
             Graphics GR = Graphics.FromImage(IMmain);
             GR.Clear(Color.Black);
@@ -45,7 +138,7 @@ namespace StarboundExport
                 int x = SBNodes[i].xcoord * ImageMult + ImageMult;
                 int y = SBNodes[i].ycoord * ImageMult + ImageMult;
                 GR.DrawRectangle(Pens.Green, x, y, ImageMult * 2, ImageMult * 2);
-                GR.DrawString(SBNodes[i].NodeType, F, Brushes.Green, x, y-15);
+                GR.DrawString(SBNodes[i].NodeType, F, Brushes.Green, x, y - 15);
                 //DrawPorts
                 for (int j = 0; j < SBNodes[i].Ports.Count; j++)
                 {
@@ -58,7 +151,7 @@ namespace StarboundExport
             //DrawWires;
             for (int i = 0; i < SBWires.Count; i++)
             {
-                int sx = SBWires[i].startx * ImageMult + ImageMult + ImageMult/2;
+                int sx = SBWires[i].startx * ImageMult + ImageMult + ImageMult / 2;
                 int sy = SBWires[i].starty * ImageMult + ImageMult + ImageMult / 2;
                 int ex = SBWires[i].endx * ImageMult + ImageMult + ImageMult / 2;
                 int ey = SBWires[i].endy * ImageMult + ImageMult + ImageMult / 2;
@@ -66,9 +159,7 @@ namespace StarboundExport
                 GR.DrawLine(Pens.Yellow, sx, sy, ex, ey);
 
             }
-            //export image
-            IMmain.Save(file + ".bmp");
-
+            return IMmain;
         }
 
         private static string GenExportFile(List<StarBoundNode> SBNodes, List<StarboundWire> SBWires)
