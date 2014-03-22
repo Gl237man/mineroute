1f5c9238aee5791f276bf0e0d66b67904ed8d3ff
Gl237man <gl237man@gmail.com>
2014-03-22 20:27:07 +0400

    Добавил базовые классы

---
+++ Mnetsynt2/Mnet.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mnetsynt2
+{
+    class Mnet
+    {
+        public List<Node> nodes;
+        public List<Wire> wires;
+
+        public void ReadMnetFile(string FileName)
+        {
+            nodes = new List<Node>();
+            wires = new List<Wire>();
+            string[] tstr = System.IO.File.ReadAllLines(FileName);
+            for (int i = 0; i < tstr.Length; i++)
+            {
+                if (tstr[i].Split(':')[0] == "NODE")
+                {
+                    Node N = new Node();
+                    N.ReadFromString(tstr[i]);
+                    nodes.Add(N);
+                }
+                if (tstr[i].Split(':')[0] == "WIRE")
+                {
+                    Wire W = new Wire();
+                    W.ReadFromString(tstr[i]);
+                    wires.Add(W);
+                }
+            }
+        }
+        public void RemoveNode(string NodeName)
+        {
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                if (nodes[i].NodeName == NodeName)
+                {
+                    nodes.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+        public void RemoveWireTo(string name,string port)
+        {
+            for (int i = 0; i < wires.Count; i++)
+            {
+                if ((wires[i].DistName == name) && (wires[i].DistPort == port))
+                {
+                    wires.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+        public void RemoveWireFrom(string name, string port)
+        {
+            for (int i = 0; i < wires.Count; i++)
+            {
+                if ((wires[i].SrcName == name) && (wires[i].SrcPort == port))
+                {
+                    wires.RemoveAt(i);
+                    return;
+                }
+            }
+        }
+        public List<Node> GetLuts()
+        {
+            List<Node> Luts = new List<Node>();
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                if (nodes[i].IsLut())
+                {
+                    Luts.Add(nodes[i]);
+                }
+            }
+            return Luts;
+        }
+        public Node FindNode(string NodeName)
+        {
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                if (nodes[i].NodeName == NodeName)
+                    return nodes[i];
+            }
+            return null;
+        }
+        internal void RenameElement(string From, string To)
+        {
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                if (nodes[i].NodeName == From)
+                    nodes[i].NodeName = To;
+            }
+            for (int i = 0; i < wires.Count; i++)
+            {
+                if (wires[i].DistName == From)
+                    wires[i].DistName = To;
+                if (wires[i].SrcName == From)
+                    wires[i].SrcName = To;
+            }
+        }
+
+        internal Wire FindWireFrom(string NodeName)
+        {
+            for (int i = 0; i < wires.Count; i++)
+            {
+                if (wires[i].SrcName == NodeName)
+                {
+                    return wires[i];
+                }
+            }
+            return null;
+        }
+
+        internal Wire FindWireFromPort(string NodeName, string PortName)
+        {
+            for (int i = 0; i < wires.Count; i++)
+            {
+                if (wires[i].SrcName == NodeName && wires[i].SrcPort == PortName)
+                {
+                    return wires[i];
+                }
+            }
+            return null;
+        }
+
+        internal Wire FindWireTo(string NodeName)
+        {
+            for (int i = 0; i < wires.Count; i++)
+            {
+                if (wires[i].DistName == NodeName)
+                {
+                    return wires[i];
+                }
+            }
+            return null;
+        }
+        internal Wire FindWireToPort(string NodeName,string PortName)
+        {
+            for (int i = 0; i < wires.Count; i++)
+            {
+                if (wires[i].DistName == NodeName && wires[i].DistPort == PortName)
+                {
+                    return wires[i];
+                }
+            }
+            return null;
+        }
+
+        internal string GetSting()
+        {
+            string ostr = "";
+            for (int i = 0; i < nodes.Count; i++)
+            {
+                ostr += nodes[i].ToString() + "\r\n";
+            }
+
+            for (int i = 0; i < wires.Count; i++)
+            {
+                ostr += wires[i].ToString() + "\r\n";
+            }
+
+            return ostr;
+        }
+    }
+}
--- Mnetsynt2/Program.cs
+++ Mnetsynt2/Program.cs
@@ -10,6 +10,42 @@ namespace Mnetsynt2
     {
         static void Main(string[] args)
         {
+            string file = "test2_D";
+
+            Mnet MainNetwork = new Mnet();
+            MainNetwork.ReadMnetFile(file + @".MNET");
+            ReducteDUP(MainNetwork);
+        }
+        private static void ReducteDUP(Mnet MainNetwork)
+        {
+            List<Node> Nodes = new List<Node>();
+            for (int i = 0; i < MainNetwork.nodes.Count; i++)
+            {
+                if (MainNetwork.nodes[i].NodeType.StartsWith("DUP"))
+                {
+                    Nodes.Add(MainNetwork.nodes[i]);
+                }
+            }
+            // DupUnion
+
+            List<string> Dnodes = new List<string>();
+            List<string> Snodes = new List<string>();
+            for (int i = 0; i < MainNetwork.wires.Count; i++)
+            {
+                if (MainNetwork.FindNode(MainNetwork.wires[i].DistName).NodeType.StartsWith("DUP"))
+                {
+                    if (MainNetwork.FindNode(MainNetwork.wires[i].SrcName).NodeType.StartsWith("DUP"))
+                    {
+                        while (MainNetwork.FindWireFrom(MainNetwork.wires[i].DistName) != null)
+                        {
+                            Wire W = MainNetwork.FindWireFrom(MainNetwork.wires[i].DistName);
+                            W.SrcName = MainNetwork.wires[i].SrcName;
+                        }
+                        Dnodes.Add(MainNetwork.wires[i].DistName);
+                        Dnodes.Add(MainNetwork.wires[i].SrcName);
+                    }
+                }
+            }
         }
     }
 }
