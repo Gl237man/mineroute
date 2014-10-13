39abbe43e1cb56080e52780176b9a689ffec285f
Вадим <gl237man@gmail.com>
2014-10-13 18:07:20 +0400

    Добавил генератор LUT тестов

---
+++ FullLutTestGen.cmd
@@ -0,0 +1,48 @@
+mkdir LUTtest
+copy vqm2MNET\bin\Debug\*.exe LUTtest\
+copy vqm2MNET\bin\Debug\*.dll LUTtest\
+copy MnetLutDecomposite\bin\Debug\*.exe LUTtest\
+copy MnetLutDecomposite\bin\Debug\*.dll LUTtest\
+copy Mnetsynt2\bin\Debug\*.exe LUTtest\
+copy Mnetsynt2\bin\Debug\*.dll LUTtest\
+copy GenerateTestForAllLut\bin\Debug\*.exe LUTtest\
+copy GenerateTestForAllLut\bin\Debug\*.dll LUTtest\
+copy BinhlEmul\bin\Debug\*.exe LUTtest\
+copy BinhlEmul\bin\Debug\*.dll LUTtest\
+copy MNETLib.BinLib LUTtest\
+del LUTtest\*.vshost.exe
+copy Templates\V2\*.binhl LUTtest\
+cd LUTtest
+mkdir 0
+mkdir 1
+mkdir 2
+mkdir 3
+mkdir 4
+mkdir 5
+mkdir 6
+mkdir 7
+mkdir 8
+mkdir 9
+mkdir A
+mkdir B
+mkdir C
+mkdir D
+mkdir E
+mkdir F
+copy *.* 0\
+copy *.* 1\
+copy *.* 2\
+copy *.* 3\
+copy *.* 4\
+copy *.* 5\
+copy *.* 6\
+copy *.* 7\
+copy *.* 8\
+copy *.* 9\
+copy *.* A\
+copy *.* B\
+copy *.* C\
+copy *.* D\
+copy *.* E\
+copy *.* F\
+GenerateTestForAllLut.exe
--- GenerateTestForAllLut/Program.cs
+++ GenerateTestForAllLut/Program.cs
@@ -10,42 +10,57 @@ namespace GenerateTestForAllLut
     {
         static void Main(string[] args)
         {
-            int lutNum = 0x5632;
+            //int lutNum = 0x5632;
             //Gen MNET
-            string MNETFile = "";
-            MNETFile += "NODE:INPort:I0" + "\r\n";
-            MNETFile += "NODE:INPort:I1" + "\r\n";
-            MNETFile += "NODE:INPort:I2" + "\r\n";
-            MNETFile += "NODE:INPort:I3" + "\r\n";
-            MNETFile += "NODE:OUTPort:nout" + "\r\n";
-            MNETFile += "NODE:OUTPort:cout" + "\r\n";
-            MNETFile += "NODE:C2LUT_"+ lutNum.ToString("X4") +"_datac:lut1" + "\r\n";
-            MNETFile += "WIRE:lut1-cout:cout-I0" + "\r\n";
-            MNETFile += "WIRE:lut1-combout:nout-I0" + "\r\n";
-            MNETFile += "WIRE:I0-O0:lut1-dataa" + "\r\n";
-            MNETFile += "WIRE:I1-O0:lut1-datab" + "\r\n";
-            MNETFile += "WIRE:I2-O0:lut1-datac" + "\r\n";
-            MNETFile += "WIRE:I3-O0:lut1-datad" + "\r\n";
-            System.IO.File.WriteAllText("lut_" + lutNum.ToString("X4") + ".MNET", MNETFile);
-            //Gen Test
-            string TestFile = "";
-            TestFile += "load ( lut_" + lutNum.ToString("X4") +"_D )" + "\r\n";
-            TestFile += "wait (5)" + "\r\n";
-            TestFile += "checkstruct()" + "\r\n";
-            TestFile += "checkio()" + "\r\n";
-            for (int i = 0; i < 16; i++)
+            for (int lutNum = 1; lutNum < 0xFFFF; lutNum++)
             {
-                int[] bits = GetBits(i);
-                TestFile += "set(I0, " + bits[0] + ")" + "\r\n";
-                TestFile += "set(I1, " + bits[1] + ")" + "\r\n";
-                TestFile += "set(I2, " + bits[2] + ")" + "\r\n";
-                TestFile += "set(I3, " + bits[3] + ")" + "\r\n";
-                TestFile += "wait(30)" + "\r\n";
-                TestFile += "read(nout)" + "\r\n";
-                TestFile += "test(nout , " + GetBits(lutNum)[i] + ")" + "\r\n";
+                string MNETFile = "";
+                MNETFile += "NODE:INPort:I0" + "\r\n";
+                MNETFile += "NODE:INPort:I1" + "\r\n";
+                MNETFile += "NODE:INPort:I2" + "\r\n";
+                MNETFile += "NODE:INPort:I3" + "\r\n";
+                MNETFile += "NODE:OUTPort:nout" + "\r\n";
+                MNETFile += "NODE:OUTPort:cout" + "\r\n";
+                MNETFile += "NODE:C2LUT_" + lutNum.ToString("X4") + "_datac:lut1" + "\r\n";
+                MNETFile += "WIRE:lut1-cout:cout-I0" + "\r\n";
+                MNETFile += "WIRE:lut1-combout:nout-I0" + "\r\n";
+                MNETFile += "WIRE:I0-O0:lut1-dataa" + "\r\n";
+                MNETFile += "WIRE:I1-O0:lut1-datab" + "\r\n";
+                MNETFile += "WIRE:I2-O0:lut1-datac" + "\r\n";
+                MNETFile += "WIRE:I3-O0:lut1-datad" + "\r\n";
+                System.IO.File.WriteAllText(lutNum.ToString("X4").Substring(0, 1) + @"\lut_" + lutNum.ToString("X4") + ".MNET", MNETFile);
+                //Gen Test
+                string TestFile = "";
+                TestFile += "load ( lut_" + lutNum.ToString("X4") + "_D )" + "\r\n";
+                TestFile += "wait (50)" + "\r\n";
+                TestFile += "checkstruct()" + "\r\n";
+                TestFile += "checkio()" + "\r\n";
+                for (int i = 0; i < 16; i++)
+                {
+                    int[] bits = GetBits(i);
+                    TestFile += "set(I0, " + bits[0] + ")" + "\r\n";
+                    TestFile += "set(I1, " + bits[1] + ")" + "\r\n";
+                    TestFile += "set(I2, " + bits[2] + ")" + "\r\n";
+                    TestFile += "set(I3, " + bits[3] + ")" + "\r\n";
+                    TestFile += "wait(120)" + "\r\n";
+                    TestFile += "read(nout)" + "\r\n";
+                    TestFile += "test(nout , " + GetBits(lutNum)[i] + ")" + "\r\n";
+                }
+                System.IO.File.WriteAllText(lutNum.ToString("X4").Substring(0,1)+ @"\lut_" + lutNum.ToString("X4") + ".emu", TestFile);
+                Console.WriteLine("lut_" + lutNum.ToString("X4") + ".emu - Generated");
             }
-            System.IO.File.WriteAllText("lut_" + lutNum.ToString("X4") + ".emu", TestFile);
             //Gen test.CMD
+            string CMDFile = "";
+            for (int lutNum = 1; lutNum < 0xFFFF; lutNum++)
+            {
+                CMDFile += "cd " + lutNum.ToString("X4").Substring(0, 1) + "\r\n";
+                CMDFile += "MnetLutDecomposite.exe " + "lut_" + lutNum.ToString("X4") + "\r\n";
+                CMDFile += "mnetsynt2.exe " + "lut_" + lutNum.ToString("X4") + "_D" + "\r\n";
+                CMDFile += "BinhlEmul.exe " + "lut_" + lutNum.ToString("X4") + ".emu" + "\r\n";
+                CMDFile += "cd .." + "\r\n";
+            }
+            System.IO.File.WriteAllText("run.cmd", CMDFile);
+
         }
 
         private static int[] GetBits(int i)
