058febfcb3c754ac406ed8851796043918dc1d3c
Вадим <gl237man@gmail.com>
2015-02-04 16:30:54 +0300

    Начал работу над генератором унарных операций.

---
+++ UopGen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UopGen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //NOT
+            //LL
+            //RR
+            //LLC
+            //RRC
+
+        }
+    }
+}
